62c4760c3a707849648ab6e141f2b4743f290302
danirc09 <100007160.joan23@fje.edu>
2022-03-10 12:31:49 +0100

    m

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -50,7 +50,7 @@ function objetoAjax() {
 }
 
 /*MOSTRAR MAPA*/
-var map = L.map('map').setView([41.373703, 2.187467], 14);
+var map = L.map('map').setView([41.373703, 2.187467], 15);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
